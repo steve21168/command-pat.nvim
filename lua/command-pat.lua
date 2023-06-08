7e04831a268e11fea9c61c743c0bc4d039867eb0
Steven Goodstein <steve21168@gmail.com>
2023-06-07 23:42:09 -0400

    Add operate on user pattern, highlight searched text, refactor

--- lua/command-pat.lua
+++ lua/command-pat.lua
@@ -1,10 +1,14 @@
 local M = {}
 
+local api = vim.api
+
 local function show(item)
   print(vim.inspect(item))
 end
 
-local api = vim.api
+local function clear_cmdline()
+  vim.cmd([[redraw | echo '']])
+end
 
 local function get_visual_selection()
   local left_pos = api.nvim_buf_get_mark(0, '<')
@@ -36,41 +40,55 @@ local function get_text_under_cursor()
   return vim.fn.expand("<cword>")
 end
 
+local function get_pattern()
+  local pattern = nil
+
+  vim.ui.input({ prompt = "Enter pattern: " }, function(pat)
+    if pat == "" or pat == nil then
+      clear_cmdline()
+      return
+    end
+    pattern = pat
+  end)
+
+  return pattern
+end
+
 local function norm_command(cmd)
   api.nvim_command('norm ' .. cmd)
 end
 
-local function clear_cmdline()
-  vim.cmd([[redraw | echo '']])
+local function mark_selection(selection_text, selection_type)
+  local line_count = api.nvim_buf_line_count(0)
+  local lines = api.nvim_buf_get_lines(0, 0, line_count, true)
+
+  local ns = api.nvim_create_namespace('command-pat')
+
+  for line_index, line in ipairs(lines) do
+    local pattern_start_idx = 0
+    while true do
+      local pattern_end_idx
+      pattern_start_idx, pattern_end_idx = string.find(line, selection_text, pattern_start_idx + 1, selection_type == 'plain')
+
+      if pattern_start_idx then
+        api.nvim_buf_set_extmark(0, ns, line_index - 1, pattern_start_idx - 1, { end_col = pattern_end_idx, hl_group = "Search" })
+      else
+        break
+      end
+    end
+  end
+
+  vim.cmd('redraw')
+  return ns
 end
 
-local function operateOnSelection(selection_text)
+local function operateOnSelection(ns)
   vim.ui.input({ prompt = "Enter norm command: " }, function(cmd)
     if cmd == "" or cmd == nil then
       clear_cmdline()
       return
     end
 
-    local line_count = api.nvim_buf_line_count(0)
-    local lines = api.nvim_buf_get_lines(0, 0, line_count, true)
-
-    local ns = api.nvim_create_namespace('command-pat')
-
-   -- Check for all occurences with pattern and run command on them
-    for line_index, line in ipairs(lines) do
-      local pattern_start_idx = 0
-      while true do
-        pattern_start_idx = string.find(line, selection_text, pattern_start_idx + 1)
-
-        if pattern_start_idx then
-          api.nvim_buf_set_extmark(0, ns, line_index - 1, pattern_start_idx - 1, {})
-        else
-          break
-        end
-      end
-    end
-
-    -- Get all exmtmarks and loop
     local extmarks = api.nvim_buf_get_extmarks(0, ns, 0, -1, {})
     for _, extmark in ipairs(extmarks) do
       local ext_id = extmark[1]
@@ -79,6 +97,7 @@ local function operateOnSelection(selection_text)
       local ext_col = extmark[2]
 
       if vim.g['command_pat_debug'] == 1 then
+        show(extmark)
         show("Ext Id: " .. ext_id)
         show("Ext line: " .. ext_line)
         show("Ext col: " .. ext_col)
@@ -94,11 +113,26 @@ local function operateOnSelection(selection_text)
 end
 
 function M.OperateOnVSelection()
-  operateOnSelection(get_visual_selection())
+  local selection_text = get_visual_selection()
+  local extmark_ns = mark_selection(selection_text, 'plain')
+  operateOnSelection(extmark_ns)
 end
 
 function M.OperateOnNSelection()
-  operateOnSelection(get_text_under_cursor())
+  local selection_text = get_text_under_cursor()
+  local extmark_ns = mark_selection(selection_text, 'plain')
+  operateOnSelection(extmark_ns)
+end
+
+function M.OperateOnPattern()
+  local pattern = get_pattern()
+
+  if pattern == nil then
+    return
+  end
+
+  local extmark_ns = mark_selection(pattern)
+  operateOnSelection(extmark_ns)
 end
 
 return M
